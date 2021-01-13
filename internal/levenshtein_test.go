955795dd78c9ee408ffdba102c52dd9bd4ba5d93
datahappy1 <43505494+datahappy1@users.noreply.github.com>
2021-01-13 23:17:18 +0100

    fixing levenshtein unit test

--- internal/levenshtein_test.go
+++ internal/levenshtein_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestLevenshteinRatio(t *testing.T) {
 	ans := LevenshteinRatio(strings.ToLower("Apple Inc."), strings.ToLower("apple Inc"))
-	if ans != 0.9473684210526315 {
-		t.Errorf("levenshteinRatio(\"Apple Inc.\", \"apple Inc\") = %g; want 0.9473684210526315", ans)
+	if ans != 0.94736844 {
+		t.Errorf("levenshteinRatio(\"Apple Inc.\", \"apple Inc\") = %g; want 0.94736844", ans)
 	}
 }
